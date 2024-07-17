28b0a9157b8638febb3544b78b0389236b3242c1
Afeni Samuel Oluwatimilehin <144561797+TimTech5@users.noreply.github.com>
2024-07-17 07:52:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelector("#menu").addEventListener("click", () => {
+  document.querySelector("nav ul").classList.toggle("showmenu");
+});
+
+
+
+
+
+
+let sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
